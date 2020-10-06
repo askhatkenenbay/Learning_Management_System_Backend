89e635ea56bd1ba56bf7678e39b8c6271789784b
askhat <askhat.kenenbay@nu.edu.kz>
2020-10-07 02:32:52 +0600

    v0.01

---
+++ login_system/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import redirect
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        print(username)
+        print(password)
+        return redirect('home')
+    return render(request,'login_system/login.html')
+
+def home(request):
+    return render(request,'login_system/home.html')
