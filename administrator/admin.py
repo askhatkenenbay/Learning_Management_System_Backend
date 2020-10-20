6116d95a6b40b0b236a8e1b5ee3daed7918053c4
DannKim <denis.kim@nu.edu.kz>
2020-10-20 20:11:21 +0600

    settings

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+# from alldata.models import *
+
+# admin.site.site_header = 'Nazarbayev University'
+# admin.site.register(School)
+# admin.site.register(Department)
+# admin.site.register(User)
+# admin.site.register(Student)
+# admin.site.register(Instructor)
+# admin.site.register(Course)
+# admin.site.register(Priority)
+# admin.site.register(Requisite)
+# admin.site.register(Registrationdate)
+# admin.site.register(Coursesection)
+# admin.site.register(Sectionday)
+# admin.site.register(Advice)
+# admin.site.register(CourseInstructor)
+# admin.site.register(StudentEnrollment)
+# admin.site.register(CourseGrades)
+# admin.site.register(Coursepagemodule)
 
 # Register your models here.
