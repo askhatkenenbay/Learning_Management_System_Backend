11d0b4758623273391bebbd5d58057ecc4a7993d
DannKim <denis.kim@nu.edu.kz>
2020-11-01 05:48:45 +0600

    asd

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.quizzes, name='quizzes'),
+    path('depression-quiz', views.depression, name='depression'),
+    path('anxiety-quiz', views.anxiety, name='anxiety'),
    
 ]
